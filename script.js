dae0d48e07b78314dc99566b42852c23e425add4
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-29 23:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+async function greetUser() {
+  const user = await fetchUser();
+  console.log(`Hello, ${user}!`); // Output after 2 seconds: Hello, John Doe!
+}
+
+greetUser();
